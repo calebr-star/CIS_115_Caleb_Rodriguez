cdfa9f542295bd97de9492948bc2c2e71a89f626
SANDHILLS\LH218-10 <jad.talbert@gmail.com>
2025-08-27 10:31:47 -0400

    mini calculator (Sort of)

---
+++ sumNumber.py
@@ -0,0 +1,14 @@
+
+
+num1 = input("Enter First Number: ")
+num2 = input("Enter Second Number: ")
+sum = float(num1) + float(num2)
+
+print("The sum of {0} and {1} is {2}".format(num1,num2,sum))
+
+
+num3 = input("Enter your 3rd Number: ")
+num4 = input("Enter your 4th number: ")
+diff = int(num4) - int(num3)
+
+print(diff)
